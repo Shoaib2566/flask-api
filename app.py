c2c3dd67d9186e50ee2350ff51e40d312aa787de
Shoaib Iqbal <Shoaibhussainiqbal135@gmail.com>
2026-05-04 02:25:47 +0500

    fixed indentation and fixed bug

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ next_id = 1
 
 @app.route('/api/health', methods=['GET'])
 def health():
-    return jsonify({'status': 'ok', 'message': 'Flask is running'}), 200@app.route('/api/students', methods=['GET'])
+    return jsonify({'status': 'ok', 'message': 'Flask is running'}), 200
+@app.route('/api/students', methods=['GET'])
 def get_students():
     return jsonify(students), 200
 
